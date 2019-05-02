5d2c0f1651f0651985b7b011e759bff06caaa1fb
AmarachiOE <ao.ejiawoko@gmail.com>
2019-05-02 16:02:27 -0700

    GET and POST requests for /api/recipes endpoint complete.

--- routers/recipes-router.js
+++ routers/recipes-router.js
@@ -16,6 +16,24 @@ recipesRouter.get("/", (req, res) => {
 });
 
 // =========== ADD RECIPES
-
+recipesRouter.post("/", (req, res) => {
+  const recipe = req.body;
+  if (!recipe || !recipe.name || !recipe.dish_id || !recipe.instructions) {
+    res.status(400).json({
+      error: "You must include a recipe with a dish_id, name, and instructions."
+    });
+  } else {
+    recipesDb
+      .addRecipe(recipe, "id")
+      .then(recipe => {
+        res.status(200).json(recipe);
+      })
+      .catch(err => {
+        res.status(500).json({
+          error: "There was an error while saving the recipe to the database"
+        });
+      });
+  }
+});
 
 module.exports = recipesRouter;
