1dfb1b79c7ce393d1b3d55eaaec1e310350bded0
AmarachiOE <ao.ejiawoko@gmail.com>
2019-05-02 15:51:58 -0700

    GET all dishes and POST new dish requests completed. GET dish by id with recipes is being problematic.

--- data/helpers/dishes-model.js
+++ data/helpers/dishes-model.js
@@ -1,22 +1,26 @@
 const db = require("../dbConfig");
 
 module.exports = {
-    getDishes,
-    getDish,
-    addDish
+  getDishes,
+  getDish,
+  addDish
 };
 
-
+// ====== getDishes() should return a list of all dishes in the database.
 function getDishes() {
-    return db('dishes');
-};
+  return db("dishes");
+}
 
+// ====== getDish(id) should return the dish with the provided id and include a list of the related recipes.
 function getDish(id) {
-    return db('dishes');
-
-};
-
+  return db("dishes as d")
+    .join("recipes as r", "r.dish_id", "d.id")
+    .select("d.id", "d.name as Dish", "r.name as Recipe")
+    .where({ id: Number(id) });
+}
+// ====== addDish(id) should add the dish to the database and return the id of the new dish.
 function addDish(dish) {
-    return db('dishes');
-
-};
+  return db("dishes")
+  .insert(dish)
+  .then(ids => ({ id: ids[0] }));
+}
--- routers/dishes-router.js
+++ routers/dishes-router.js
@@ -15,10 +15,45 @@ dishesRouter.get("/", (req, res) => {
     });
 });
 
-// =========== GET DISHES BY ID
+// =========== GET DISHES BY ID (including Recipe List)
+dishesRouter.get("/:id", (req, res) => {
+  const dishId = req.params.id;
+  dishesDb
+    .getDish(dishId)
+    .then(dish => {
+      if (dish) {
+        res.status(200).json(dish);
+      } else {
+        res.status(404).json({ error: "This dish could not be found." });
+      }
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: "This dish's information could not be retrieved." });
+    });
+});
 
 // =========== ADD DISHES
-
+dishesRouter.post("/", (req, res) => {
+const dish = req.body;
+if (!dish || !dish.name) {
+  res.status(400).json({
+    error: "You must include a dish with a name."
+  });
+} else {
+  dishesDb
+    .addDish(dish, "id")
+    .then(dish => {
+      res.status(200).json(dish);
+    })
+    .catch(err => {
+      res.status(500).json({
+        error: "There was an error while saving the dish to the database"
+      });
+    });
+}
+});
 
 
 module.exports = dishesRouter;
