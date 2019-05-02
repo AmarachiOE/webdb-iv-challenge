4481f0689df62aa6c510edd38df857f0dd408334
AmarachiOE <ao.ejiawoko@gmail.com>
2019-05-02 13:27:13 -0700

    Migration created for ingredients table

--- data/migrations/20190502124348_dishes.js
+++ data/migrations/20190502124348_dishes.js
@@ -1,10 +1,9 @@
-
 exports.up = function(knex, Promise) {
   return knex.schema.createTable("dishes", tbl => {
-      tbl.increments();
-      tbl.string("name", 128).notNullable();
-      tbl.timestamps(true, true);
-  })
+    tbl.increments();
+    tbl.string("name", 128).notNullable();
+    tbl.timestamps(true, true);
+  });
 };
 
 exports.down = function(knex, Promise) {
